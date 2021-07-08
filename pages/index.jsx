2dab2ce97f718a32af7750b74fc59cd217219f90
Mattia <mattia@wonderflow.ai>
2021-07-08 09:51:21 +0200

    chore: update react-components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import { Meta } from '@/components/meta'
 import { Text } from '@/components/text'
-import { Button } from '@wonderflow/react-components/dist'
+import { Button } from '@wonderflow/react-components'
 import React from 'react'
 
 export default function Home ({ props }) {
