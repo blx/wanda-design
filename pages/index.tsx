5c49683c204f460e8634e7dd148eaa37868a0eb6
Mattia <mattia@wonderflow.ai>
2021-07-19 11:46:41 +0200

    fix: add temp index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
-import { Shell } from '@/components/layouts/shell'
 import React from 'react'
+import { Stack } from '@wonderflow/react-components'
+import { Logo } from '@/components/logo'
 
 export const Home = () => {
   return (
-    <Shell>
-      A
-    </Shell>
+    <Stack
+      direction="row"
+      style={{
+        height: '100vh'
+      }}
+      horizontalAlign="center"
+      verticalAlign="center"
+    >
+      <Logo style={{
+        blockSize: '2.66rem',
+        inlineSize: '9.83rem'
+      }}
+      />
+    </Stack>
   )
 }
 
