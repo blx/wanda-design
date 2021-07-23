4ccc89268be288200cf06f6ce75e1541ccc2e062
Mattia <mattia@wonderflow.ai>
2021-07-23 13:58:32 +0200

    chore: clean env var

--- types/main.d.ts
+++ types/main.d.ts
@@ -15,9 +15,6 @@ declare namespace NodeJS {
     /* eslint-disable @typescript-eslint/naming-convention */
     IS_DEV: boolean;
     NEXT_PUBLIC_DOMAIN: string;
-    CONTENTFUL_SPACE_ID: string;
-    CONTENTFUL_CDA_TOKEN: string;
-    CONTENTFUL_CPA_TOKEN: string;
   }
 }
 
