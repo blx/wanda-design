00fbbe8cdab3ced775aba717a828bdf6b25ea54f
Mattia <mattia@wonderflow.ai>
2021-07-23 12:57:05 +0200

    chore: update navigation

--- src/navigation.ts
+++ src/navigation.ts
@@ -40,5 +40,52 @@ export default [
         ]
       }
     ]
+  },
+  {
+    title: 'Develop',
+    items: [
+      {
+        path: '/develop/design-tokens',
+        label: 'Design tokens',
+        target: ''
+      },
+      {
+        path: '/develop/styles',
+        label: 'Styles',
+        target: ''
+      },
+      {
+        path: '/develop/themes',
+        label: 'Themes',
+        target: ''
+      },
+      {
+        path: '/develop/configurations',
+        label: 'Configurations',
+        target: ''
+      }
+    ]
+  },
+  {
+    title: '',
+    items: [
+      {
+        label: 'Components',
+        expandable: true,
+        target: '',
+        items: [
+          {
+            path: '/develop/components/button',
+            label: 'Button',
+            target: ''
+          },
+          {
+            path: '/develop/components/icon-button',
+            label: 'Icon button',
+            target: ''
+          }
+        ]
+      }
+    ]
   }
 ]
