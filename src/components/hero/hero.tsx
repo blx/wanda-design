4a2b185ace1fbb6be9ef2c5ffdbdc8f55699f285
Mattia <mattia@wonderflow.ai>
2021-08-17 11:34:09 +0200

    chore: add hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,13 +1,16 @@
 import { Stack, Text, Title } from '@wonderflow/react-components'
 import React from 'react'
-import { Hero as HeroClass, Tagline } from './hero.module.css'
+import { Hero as HeroClass, Video, Content } from './hero.module.css'
 
 export const Hero = () => {
   return (
     <Stack className={HeroClass} verticalAlign="end" fill={false}>
-      <Stack rowGap={16}>
+      <video className={Video} autoPlay loop muted playsInline height="220" width="390">
+        <source src="/hero-video.mp4" type="video/mp4" />
+      </video>
+      <Stack rowGap={16} className={Content}>
         <Title level="display">Wanda</Title>
-        <Text size={28} className={Tagline}>
+        <Text size={28}>
           Wanda is Wonderflow’s open-source design system built for digital products and experiences.
         </Text>
       </Stack>
