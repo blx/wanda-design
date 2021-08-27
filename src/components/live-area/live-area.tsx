ebcc3da77c920926b38604abb425bedd3c569bbb
Mattia <astorino.mattia@gmail.com>
2021-08-27 12:24:28 +0200

    chore: various fixes

--- src/components/live-area/live-area.tsx
+++ src/components/live-area/live-area.tsx
@@ -1,26 +1,37 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, useRef } from 'react'
 import clsx from 'clsx'
 import { Stack } from '@wonderflow/react-components'
+import { useMouse } from 'react-use'
 
 import { LiveArea as LiveAreaClass } from './live-area.module.css'
 import { StackProps } from '@wonderflow/react-components/stack'
 
 type LiveAreaProps = PropsWithClass & StackProps & {
   maxHeight?: string;
+  live?: boolean;
 }
 
 export const LiveArea: React.FC<LiveAreaProps> = ({
   children,
   maxHeight,
   style,
+  live = true,
   ...props
 }) => {
+  const ref = useRef(null)
+  const { docX, docY, posX, posY } = useMouse(ref)
+
+  const hoverCoordinates = (docValue: number = 0, elValue: number = 0) => ((docValue - elValue) - 200).toFixed(2)
+
   const dynamicStyle: CSSProperties = {
-    '--maxHeight': maxHeight
+    '--maxHeight': maxHeight,
+    '--hoverX': `${hoverCoordinates(docX, posX)}px`,
+    '--hoverY': `${hoverCoordinates(docY, posY)}px`
   }
 
   return (
     <Stack
+      ref={ref}
       className={clsx(LiveAreaClass)}
       wrap
       fill={false}
@@ -30,6 +41,7 @@ export const LiveArea: React.FC<LiveAreaProps> = ({
       columnGap={16}
       rowGap={16}
       style={{ ...dynamicStyle, ...style }}
+      data-live-area-live={live}
       {...props}
     >
       {children}
