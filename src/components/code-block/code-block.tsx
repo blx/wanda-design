f40529d554f58f5542ef87a02c561228aef477d8
Mattia <astorino.mattia@gmail.com>
2021-09-03 09:09:16 +0200

    chore: add language to code-block

--- src/components/code-block/code-block.tsx
+++ src/components/code-block/code-block.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/no-array-index-key */
-import { Button } from '@wonderflow/react-components'
+import { Button, Text, Stack } from '@wonderflow/react-components'
 import clsx from 'clsx'
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
 import React, { useCallback, useRef } from 'react'
 import { useCopyToClipboard } from 'react-use'
 
-import { CodeBlock as CodeBlockClass, Action, Code } from './code-block.module.css'
+import { CodeBlock as CodeBlockClass, Action, Code, Toolbar } from './code-block.module.css'
 import theme from './wonder-theme'
 
 type CodeBlockProps = {
@@ -34,14 +34,17 @@ export const CodeBlock: React.FC<CodeBlockProps> = ({
           className, style, tokens, getLineProps, getTokenProps
         }) => (
           <>
-            <Button
-              className={Action}
-              dimension="small"
-              kind="flat"
-              onClick={copyContent()}
-            >
-              Copy
-            </Button>
+            <Stack direction="row" fill={false} horizontalAlign="space-between" verticalAlign="center" className={Toolbar}>
+              {language && <Text fluid={false} size={14} dimmed={6}>{language}</Text>}
+              <Button
+                className={Action}
+                dimension="small"
+                kind="flat"
+                onClick={copyContent()}
+              >
+                Copy
+              </Button>
+            </Stack>
 
             <pre className={clsx(Code, className)} style={{ ...style }}>
               <div ref={CodeRef}>
