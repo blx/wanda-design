9b57c4f1ec3e4e43dba15bc93ff383ca667017cd
Mattia <astorino.mattia@gmail.com>
2021-09-21 16:23:36 +0200

    fix(navigation): improve rendering and animations

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useCallback, useRef } from 'react'
+import React, { Fragment, useCallback, useMemo } from 'react'
 import { Tree } from '@/components/tree'
 import { Pill } from '@/components/pill'
 import { Separator, Stack, Text } from '@wonderflow/react-components'
@@ -10,7 +10,6 @@ type NavigationProps = {
 };
 
 export const Navigation = ({ data }: NavigationProps) => {
-  const menuRef = useRef<any>(null)
   const router = useRouter()
 
   const includesPath = useCallback(
@@ -18,60 +17,73 @@ export const Navigation = ({ data }: NavigationProps) => {
     [router.asPath]
   )
 
+  const navigationLink = useCallback(
+    (url: string, label: string) => <a aria-current={includesPath(url) ? 'page' : undefined}>{label}</a>,
+    [includesPath]
+  )
+
+  const renderSubMenu = useCallback(
+    (menu: NavigationItem) => menu.items?.map(subItem => (
+      <Tree.Li key={subItem.path}>
+        {subItem.wip
+          ? (
+            <Text as="span" size={16} dimmed={5}>
+              <Stack as="span" direction="row" columnGap={8} inline verticalAlign="center">
+                {subItem.label}
+                <Pill>soon</Pill>
+              </Stack>
+            </Text>
+            )
+          : (
+            <Link href={subItem.path}>
+              {navigationLink(subItem.path, subItem.label)}
+            </Link>
+            )}
+      </Tree.Li>
+    )),
+    []
+  )
+
+  const renderGroup = useCallback(
+    (group: NavigationItem | NavigationGroup) => group.items?.map(link => (
+      <Tree.Menu
+        key={link.label}
+        open={link.items?.some(item => includesPath(item.path))}
+        expandable={!!link.items}
+        summary={!!link.items && link.label}
+      >
+        {link.items
+          ? renderSubMenu(link)
+          : (
+            <Tree.Li>
+              <Link href={link.path}>
+                {navigationLink(link.path, link.label)}
+              </Link>
+            </Tree.Li>
+            )
+        }
+      </Tree.Menu>
+    )),
+    [navigationLink, includesPath, renderSubMenu]
+  )
+
+  const renderNavigation = useMemo(() => data.map((group, index) => {
+    return (
+      <Fragment key={group.title}>
+
+        {index !== 0 && <Separator />}
+
+        <Tree.Group title={group.title}>
+          {renderGroup(group)}
+        </Tree.Group>
+      </Fragment>
+    )
+  }), [data, renderGroup])
+
   return (
-    <nav ref={menuRef}>
+    <nav>
       <Stack fill={false} horizontalAlign="stretch" rowGap={24}>
-        {
-        data.map((group, index) => {
-          return (
-            <Fragment key={group.title}>
-
-              {index !== 0 && <Separator />}
-
-              <Tree.Group title={group.title}>
-                {
-                group.items.map(link => (
-                  <Tree.Menu
-                    key={link.label}
-                    open={link.items?.some(item => includesPath(item.path))}
-                    expandable={!!link.items}
-                    summary={!!link.items && link.label}
-                  >
-                    {link.items
-                      ? link.items.map(subItem => (
-                        <Tree.Li key={subItem.path}>
-                          {subItem.wip
-                            ? (
-                              <Text as="span" size={16} dimmed={5}>
-                                <Stack as="span" direction="row" columnGap={8} inline verticalAlign="center">
-                                  {subItem.label}
-                                  <Pill>soon</Pill>
-                                </Stack>
-                              </Text>
-                              )
-                            : (
-                              <Link href={subItem.path}>
-                                <a aria-current={includesPath(subItem.path) ? 'page' : undefined}>{subItem.label}</a>
-                              </Link>
-                              )}
-                        </Tree.Li>
-                      ))
-                      : (
-                        <Tree.Li>
-                          <Link href={link.path}>
-                            <a aria-current={includesPath(link.path) ? 'page' : undefined}>{link.label}</a>
-                          </Link>
-                        </Tree.Li>
-                        )
-                    }
-                  </Tree.Menu>
-                ))
-              }
-              </Tree.Group>
-            </Fragment>
-          )
-        })
-      }
+        {renderNavigation}
       </Stack>
     </nav>
   )
