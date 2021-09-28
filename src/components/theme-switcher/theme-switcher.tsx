8410675d2021b98a016ff60c6d3e382839298c50
Mattia <astorino.mattia@gmail.com>
2021-09-28 08:38:18 +0200

    fix: change to resolved theme

--- src/components/theme-switcher/theme-switcher.tsx
+++ src/components/theme-switcher/theme-switcher.tsx
@@ -3,9 +3,9 @@ import { useTheme } from 'next-themes'
 import { ThemeSwitch } from '@wonderflow/react-components'
 
 export const ThemeSwitcher = () => {
-  const { theme, setTheme } = useTheme()
+  const { resolvedTheme, setTheme } = useTheme()
 
   return (
-    <ThemeSwitch onChange={({ currentTarget }) => setTheme(currentTarget.value)} value={theme} currentTheme={theme} />
+    <ThemeSwitch onChange={({ currentTarget }) => setTheme(currentTarget.value)} value={resolvedTheme} currentTheme={resolvedTheme} />
   )
 }
